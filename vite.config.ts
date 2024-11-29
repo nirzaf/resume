d1339a5cd196610e5a0ea9fe2527a38140e09c5c
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-11-29 04:33:49 +0300

    🛠️ .github/workflows/static.yml -> Updated workflow for React 🛠️ vite.config.ts -> Added base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/resume/', 
   plugins: [react()],
   assetsInclude: ['**/*.jpg', '**/*.JPG', '**/*.jpeg', '**/*.JPEG', '**/*.png', '**/*.PNG', '**/*.gif', '**/*.svg'],
   resolve: {
